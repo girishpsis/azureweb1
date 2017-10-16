d4d63af8ebe0c40f21d4841287c7e0b05e335ef8
papfan <31174429+girishpsis@users.noreply.github.com>
2017-10-15 23:44:35 -0700

    first
    first

---
+++ WebSite1/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox runat="server"  ID="lblOne" TextMode="MultiLine" Height="400px" Width="700px" Text="hello mr periera"/>
+        </div>
+        <div>
+            <asp:Button runat="server" ID="btnSubmit" OnClick="btnSubmit_Click" Text="Send Email"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSite1/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       
+
+    }
+
+    static void SendEMail(string text)
+    {
+        var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+        var client = new SendGridClient(apiKey);
+        var msg = new SendGridMessage()
+        {
+            From = new EmailAddress("azure_5ba432e313a50299f93a5b151de6c895@azure.com", "Girish Nair"),
+            Subject = "Hello Deepthi",
+            PlainTextContent = text,
+            HtmlContent = "<strong>" + text + "</strong>"
+        };
+        msg.AddTo(new EmailAddress("deepthimj008acm@gmail.com", "Deepthi Girish"));
+        msg.AddTo(new EmailAddress("girishpsis@gmail.com", "Girish Nair"));
+        var response = client.SendEmailAsync(msg);
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        SendEMail(lblOne.Text);
+    }
+}
