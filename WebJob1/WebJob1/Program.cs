bd6e5b3401123e0bc466b20888aa296108f28a95
papfan <31174429+girishpsis@users.noreply.github.com>
2017-10-18 22:22:33 -0700

    changes

--- WebJob1/WebJob1/Program.cs
+++ WebJob1/WebJob1/Program.cs
@@ -8,6 +8,9 @@ using System.Diagnostics;
 using Microsoft.ServiceBus.Messaging;
 using SendGrid;
 using SendGrid.Helpers.Mail;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
 
 namespace WebJob1
 {
@@ -20,25 +23,55 @@ namespace WebJob1
         {
             var connectionString = "Endpoint=sb://servicebus-gps.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=JntuuS514ao9BrDtaTsx/q0GL305KrjRcdI4/8fi7nI=";
             var queueName = "GirishQueue";
-
+            Console.WriteLine("web job 1 started at " + DateTime.Now.ToString());
             var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
 
-            client.OnMessage(message =>
-            {
-                string msg = message.GetBody<String>();
-               Console.WriteLine(String.Format("Message body: {0}", msg));
-                //Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
-                //SendEmail(msg);
-            });
+            BrokeredMessage message = client.Receive();
+            string messageBody = message.GetBody<string>();
+            Console.WriteLine($"Message received: {messageBody}");
+            SendEmail(messageBody);
+            message.Complete();
+            //var credentials = new StorageCredentials("servicebus-gps", "JntuuS514ao9BrDtaTsx/q0GL305KrjRcdI4/8fi7nI=");
 
-           
+            //var storageAccount = new CloudStorageAccount(credentials, true);
+
+            //// Create a new client
+            //CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            //// Retrieve a reference to a queue
+            //CloudQueue queue = queueClient.GetQueueReference("GirishQueue");
+
+            //foreach (CloudQueueMessage message in queue.GetMessages(20, TimeSpan.FromMinutes(5)))
+            //{
+            //    // Reading content from message
+            //    Console.WriteLine(message.AsString);
+
+            //    // Process all messages in less than 5 minutes, deleting each message after processing.
+            //    queue.DeleteMessage(message);
+            //}
+
+            //client.OnMessage(message =>
+            //{
+            //    string msg = message.GetBody<String>();
+            //   Console.WriteLine(String.Format("Message body 6: {0}", msg));
+            //    //Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+            //    Program pgm = new Program();
+            //   pgm.SendEmail(msg);
+            //});
+
+
         }
 
-        static void SendEmail(string text)
+       
+
+        public static void SendEmail(string text)
         {
             try
             {
+                Console.WriteLine("Send email started");
+
                 var apiKey = System.Environment.GetEnvironmentVariable("SENDGRID_APIKEY");
+                Console.WriteLine(apiKey);
                 var client = new SendGridClient(apiKey);
                 var msg = new SendGridMessage()
                 {
@@ -54,6 +87,10 @@ namespace WebJob1
             catch(Exception ex)
             {
                 Console.WriteLine("Exception - " + ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Send email done!");
             }
         }
     }
